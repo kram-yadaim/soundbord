1f9ded5175d97c53510f533a9c0c98c1304f72a6
פלג גואטה <peleg2164@gmail.com>
2024-12-18 17:10:22 +0200

    one

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View , TouchableOpacity} from 'react-native';
+import { Audio } from 'expo-av';
+
 
 export default function App() {
+  const playSound = async (filePath) => {
+    try {
+      const { sound } = await Audio.Sound.createAsync(filePath);
+      await sound.playAsync();
+
+      // Cleanup after sound finishes
+      sound.setOnPlaybackStatusUpdate((status) => {
+        if (status.didJustFinish) {
+          sound.unloadAsync(); // Unload the sound to free resources
+        }
+      });
+    } catch (error) {
+      console.error('Error playing sound:', error);
+    }
+  };
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity style={styles.button} onPress={() => playSound(require('./assets/sounds/one.mp3'))}>
+        <Text style={styles.button_text}>V8</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => playSound(require('./assets/sounds/no_no.mp3'))}>
+        <Text style={styles.button_text}>no wait</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => playSound(require('./assets/sounds/hours.mp3'))}>
+        <Text style={styles.button_text}>סוס</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.button_text}>ריק</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} >
+        <Text style={styles.button_text}>ריק</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} >
+        <Text style={styles.button_text}>ריק</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} >
+        <Text style={styles.button_text}>ריק</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity style={styles.button} onPress={() => playSound(require('./assets/sounds/TWO.mp3'))}>
+        <Text style={styles.button_text}>משהו מוזר</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,5 +58,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button:{
+    backgroundColor: '#4797ff',
+    borderRadius: 10,
+    padding:15,
+    paddingHorizontal:150,
+    marginBottom:18,
+  },
+  button_text:{
+    color:"white",
+    fontWeight:"900",
+    fontSize:20,
   },
 });
